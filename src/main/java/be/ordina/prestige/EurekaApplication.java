dc2d6b8d6e4400e21db6620a9ee8ecbcad8627cf
Joris Lefever <joris.lefever@ordina.be>
2017-02-09 14:08:55 +0100

    Adding config to use Docker and config server

--- src/main/java/be/ordina/prestige/EurekaApplication.java
+++ src/main/java/be/ordina/prestige/EurekaApplication.java
@@ -3,9 +3,11 @@ package be.ordina.prestige;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableRetry
 public class EurekaApplication {
 
 	public static void main(String[] args) {
